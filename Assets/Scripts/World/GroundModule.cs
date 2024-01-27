1d60cb7182ae0e3ccf76bb1bfe985bc90fc2e509
Philipp Irmscher <82280738+philirmscher@users.noreply.github.com>
2024-01-27 12:43:23 +0100

    feat: improved worldgenerator with additinal modules and a rulesysteme

--- Assets/Scripts/World/GroundModule.cs
+++ Assets/Scripts/World/GroundModule.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GroundModule : MonoBehaviour
 {
+    [SerializeField]
+    private List<ChildModuleInfo> childObjects = new List<ChildModuleInfo>(); // Liste der möglichen Nachfolger-Module
+
+    public List<ChildModuleInfo> ChildObjects
+    {
+        get { return childObjects; }
+    }
+
     // Ein öffentlicher Ankerpunkt für dieses Objekt
     public Transform anchorPoint;
 
@@ -12,5 +21,18 @@ public class GroundModule : MonoBehaviour
             return anchorPoint.position;
         }
         return transform.position; // Wenn kein Ankerpunkt zugewiesen ist, verwenden Sie die Position des Objekts selbst
+    }
+}
+
+[System.Serializable]
+public class ChildModuleInfo
+{
+    public GameObject childObject; // Das mögliche Nachfolger-Modul
+    public int priority = 1; // Priorität des Nachfolger-Moduls (Bereich von 1-10)
+
+    public int Priority
+    {
+        get { return priority; }
+        set { priority = Mathf.Clamp(value, 0, 100); }
     }
 }
