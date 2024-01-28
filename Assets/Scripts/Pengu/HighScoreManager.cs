39388a7e1318dfaaf92ddd5fc6b104fefc039945
jazonthemazon <97111436+jazonthemazon@users.noreply.github.com>
2024-01-28 09:50:54 +0100

    added highscore saving

--- Assets/Scripts/Pengu/HighScoreManager.cs
+++ Assets/Scripts/Pengu/HighScoreManager.cs
@@ -6,12 +6,53 @@ public class HighScoreManager : MonoBehaviour
 {
     [SerializeField] int meterPerPoint = 10;
     [SerializeField] private TMPro.TMP_Text scoreText;
+    [SerializeField] private TMPro.TMP_Text highScoreText;
     [SerializeField] private Transform playerTransform;
     private int currentScore = 0;
+    private int highScore;
+
+    public static HighScoreManager instance { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+
+        if (!PlayerPrefs.HasKey("highScore"))
+        {
+            PlayerPrefs.SetInt("highScore", 0);
+            LoadHighScore();
+        }
+        else
+        {
+            LoadHighScore();
+        }
+    }
+
+    private void Start()
+    {
+        highScoreText.text = highScore.ToString();
+    }
 
     private void FixedUpdate()
     {
         currentScore = Mathf.Max(currentScore, (int)playerTransform.position.z / meterPerPoint);
         scoreText.text = currentScore.ToString();
+        Debug.Log(highScore);
+    }
+
+    public void SetHighScore()
+    {
+        highScore = Mathf.Max(highScore, currentScore);
+        SaveHighScore();
+    }
+
+    private void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("highScore");
+    }
+
+    private void SaveHighScore()
+    {
+        PlayerPrefs.SetInt("highScore", highScore);
     }
 }
