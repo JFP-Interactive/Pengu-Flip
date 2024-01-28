89fbce157968eec47e03daf133d02ab71257a341
Philipp Irmscher <82280738+philirmscher@users.noreply.github.com>
2024-01-28 10:34:30 +0100

    feat: added bomb explosion

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -6,19 +6,29 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private float bombStrength = 10000f;
+    [SerializeField] private ParticleSystem explosionParticles; 
+
     private void OnTriggerEnter(Collider other)
     {
         var player = other.gameObject.GetComponent<Rigidbody>();
         SpeedController.Instance.maxSpeed = 100f;
-        //boost away from bomb
         player.velocity = Vector3.zero;
         var direction = player.transform.position - transform.position;
         player.AddForce(direction.normalized * bombStrength);
         player.AddForce(Vector3.up * bombStrength);
+
+        if (explosionParticles != null)
+        {
+            ParticleSystem explosion = Instantiate(explosionParticles, transform.position, Quaternion.identity);
+            explosion.Play();
+
+            Destroy(explosion.gameObject, explosion.main.duration);
+        }
+
         StartCoroutine(ResetSpeed());
         Destroy(gameObject);
     }
-    
+
     private IEnumerator ResetSpeed()
     {
         yield return new WaitForSeconds(2f);
