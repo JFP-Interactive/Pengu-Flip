a2df244cf3d887614f839d6cb722b5cfd54329ad
Chafficui <felixbeinssen@gmail.com>
2024-01-28 12:06:12 +0100

    feat: highscore in menu

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private TMPro.TMP_Text highscoreText;
+    
+    private void Start()
+    {
+        highscoreText.text = PlayerPrefs.GetInt("highScore", 0).ToString();
+    }
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
